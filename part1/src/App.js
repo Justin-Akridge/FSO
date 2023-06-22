f2bbf002aed09a518d8e9be8e159768d1be87d85
Justin Akridge <Akridjus@gmail.com>
2023-06-22 16:40:42 -0400

    part1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const Header = ({ course }) => {
     return (
         <h1>{course}</h1>
@@ -29,7 +31,7 @@ const Total = ({ parts }) => {
     )
 }
 
-const App = () => {
+const Class = () => {
     const course = {
         name: 'Half Stack application development',
         parts: [
@@ -57,4 +59,82 @@ const App = () => {
     )
 }
 
+const Hello = ({ name, age }) => {
+
+    const bornYear = () => new Date().getFullYear() - age;
+
+    return (
+        <div>
+            <p>
+                Hello {name}, you are {age} years old
+                Your age is probably {bornYear()}
+            </p>
+        </div>
+    )
+}
+  
+const Display = ({count}) => <div>{count}</div>
+
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                The app is used by pressing buttons
+            </div>
+        )
+    }
+    return (
+        <div>
+            button press history: {props.allClicks.join(' ')}
+        </div>
+    )
+}
+
+const App = () => {
+    const [right, setRight] = useState(0)
+    const [left, setLeft] = useState(0)
+    const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState(0)
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        const updateleft = left + 1
+        setLeft(updateleft)
+        setTotal(updateleft + right)
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        const updateright = right + 1
+        setRight(updateright)
+        setTotal(left + updateright)
+    }
+
+    const [word, setWord] = useState('')
+   
+    const hello = (name) => {
+        const handle = () => {
+            if (word === "") {
+                setWord(name)
+            } else {
+                setWord("")
+            }
+        }
+        return handle
+    }
+    return (
+        <>
+            {left}
+
+            <Button handleClick={handleLeftClick} text='Left' />
+            <Button handleClick={handleRightClick} text='Right' />
+            {right}
+            <History allClicks={allClicks} />
+            <p>total {total}</p>
+            <p>{word}</p>
+            <Button handleClick={hello("justin")} text='word' />
+        </>
+    )
+}
 export default App
