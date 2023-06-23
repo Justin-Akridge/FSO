acfea4341e8367b76533d65a9d3bd6f36883b466
Justin Akridge <Akridjus@gmail.com>
2023-06-22 21:48:53 -0400

    part1

--- part1/src/App.js
+++ part1/src/App.js
@@ -63,7 +63,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [total, setTotal] = useState(0)
-
   const handleGood = () => {
     const update = good + 1
     setGood(update)
@@ -82,6 +81,30 @@ const App = () => {
     setTotal(total + 1)
   }
 
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  console.log(votes)
+  const handleSelect = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const handleVotes = () => {
+    const arr = [...votes]
+    arr[selected] += 1
+    setVotes(arr)
+  }
+
   return (
     <>
       <h1>give feedback</h1>
@@ -90,6 +113,13 @@ const App = () => {
       <Button handleSubmit={handleBad} text="bad"/>
       <Statistics title="statistics" good={good} neutral={neutral} bad={bad} total={total} />
       
+      <h1>Anecedote of the day</h1> 
+      <p>{anecdotes[selected]} has {votes[selected]} votes</p>
+      <button onClick={handleVotes}>Vote</button>
+      <button onClick={handleSelect}>Next Quote</button>
+
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[votes.indexOf(Math.max(...votes))]} has {Math.max(...votes)} votes.</p>
     </>
   )
 }
