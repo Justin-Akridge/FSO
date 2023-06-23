3b15a207aaf666f05b07b594ef728ac21386cc62
Justin Akridge <Akridjus@gmail.com>
2023-06-23 13:56:06 -0400

    arbitrary number of courses added

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,13 +1,11 @@
-const Course = ({ course }) => {
-    const initialValue = 0
-    const {parts} = course
-    const total = course.parts.reduce((s, p) => (s + p.exercises), initialValue)
+const Course = ({ course, parts }) => {
+    const total = parts.reduce((s, p) => (s + p.exercises)) 
 
     return (
         <>
-            <h1>{course.name}</h1> 
+            <h1>{course}</h1> 
             <ul>
-                {course.parts.map(part => {
+                {parts.map(part => {
                     return (
                         <li key={part.id}>{part.name} {part.exercises}</li>
                     )
@@ -17,36 +15,61 @@ const Course = ({ course }) => {
         </>
     )
 }
+
 const App = () => {
-    const course = {
-      id: 1,
-      name: 'Half Stack application development',
-      parts: [
+    const courses = [
         {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        }, 
         {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-            name: 'Redux',
-            exercises: 11,
-            id: 4
-        },
-      ]
-    }
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
   
     return (
-        <Course course={course} />
+        <>
+            {courses.map(course => {
+                return (
+                    <Course key={course.id} course={course.name} parts={course.parts} />
+                )
+            })} 
+        </>
     )
   }
   
