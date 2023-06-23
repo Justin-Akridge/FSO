4735e19d2491e80d6ea663339d1ff831e62d8900
Justin Akridge <Akridjus@gmail.com>
2023-06-23 11:17:25 -0400

    changes

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,23 +1,53 @@
-const Note = ({ note }) => {
-    return (
-        <li>{note}</li>
-    )
-}
+const Course = ({ course }) => {
+    const initialValue = 0
+    const {parts} = course
+    const total = course.parts.reduce((s, p) => (s + p.exercises), initialValue)
 
-const App = (props) => {
-    const { notes } = props
-    
     return (
         <>
-            <h1>Notes</h1>
+            <h1>{course.name}</h1> 
             <ul>
-                {notes.map(note => 
-                    <Note key={note.id} note={note.content} />
-                )}
+                {course.parts.map(part => {
+                    return (
+                        <li key={part.id}>{part.name} {part.exercises}</li>
+                    )
+                })}
             </ul>
+            <h4>Total number of exercises {total}</h4>
         </>
     )
-    
 }
-
-export default App
+const App = () => {
+    const course = {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+        },
+      ]
+    }
+  
+    return (
+        <Course course={course} />
+    )
+  }
+  
+  export default App
